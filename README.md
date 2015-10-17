8b458b62c1e4dcf2ac90c3c3418ad3dd66f66c1b
Bao Pham <gbaopham@gmail.com>
2015-10-17 03:24:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Quickly consume SQS events.
+
+1)
+```
+# .env
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=
+QUEUE_URL=
+```
+
+2) `npm install`
+
+3) `node app.js`
---
+++ app.js
@@ -0,0 +1,16 @@
+var Consumer = require('sqs-consumer');
+require('dotenv').load();
+
+var app = Consumer.create({
+  queueUrl: process.env.QUEUE_URL,
+  handleMessage: function (message, done) {
+    console.log(message);
+    done();
+  }
+});
+
+app.on('error', function (err) {
+  console.log(err.message);
+});
+
+app.start();
